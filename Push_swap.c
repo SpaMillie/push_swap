5829bc83731146951345482dd8de4c57496aaf3f
Milica  Spasic <militsaspasic@gmail.com>
2024-03-04 16:19:28 +0200

    made some progress

--- Push_swap.c
+++ Push_swap.c
@@ -6,7 +6,7 @@
 /*   By: mspasic <mspasic@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 17:57:41 by mspasic           #+#    #+#             */
-/*   Updated: 2024/03/01 20:04:16 by mspasic          ###   ########.fr       */
+/*   Updated: 2024/03/04 16:05:27 by mspasic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,14 +30,6 @@ int	error_message(void)
 	write(2, "Error", 6);
 	write(1, '\n', 1);
 	return (0);
-}
-
-void	init_params(t_params *params, int argc)
-{
-	params->list = NULL;
-	params->check = 0;
-	params->counter = 0;
-	params->args = argc;
 }
 
 int	check_str(char *str)
@@ -91,25 +83,28 @@ int	check_if_valid(char **str, t_params *params)
 	}
 }
 
+void	init_params(t_params *params, int argc)
+{
+	params->list = NULL;
+	params->check = 0;
+	params->counter = 0;
+	params->args = argc;
+}
+
 int	main(int argc, char **argv)
 {
-	int			i;
 	t_params	*params;
 
-	init_params(params, argc);
 	if (argc >= 2)
 	{
-		i = 0;
+		init_params(params, argc);
 		params->check = check_if_valid (argv, params);
 		if (params->check != 0)
 			return (error_message());
-		// while (params->args > 1)
-		// {
-		// 	params->list[i] = ft_atoi(argv[i]);
-		// 	i++;
-		// }
-		// push_swap(params->list);
+		params->check = create_a();
+		if(params->check != 0)
+			return (error_message());
 	}
 	else
-		return(error_message());
+		return (error_message());
 }
