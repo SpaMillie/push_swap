af4bb475cd9778741a574f8466c07e7f92efd93b
Milica  Spasic <militsaspasic@gmail.com>
2024-03-11 15:42:41 +0200

    one malloc adjustment

--- swap_and_push.c
+++ swap_and_push.c
@@ -6,7 +6,7 @@
 /*   By: mspasic <mspasic@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 17:35:32 by mspasic           #+#    #+#             */
-/*   Updated: 2024/03/11 14:04:00 by mspasic          ###   ########.fr       */
+/*   Updated: 2024/03/11 15:41:24 by mspasic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,47 +41,24 @@ void	swap_both(int *a, int *b, t_params *params)
 	write(1, '\n', 1);
 }
 
-int	*push_temp_1(int *stack_1, int*stack_2, t_params *params, int length)
+void	push_to(int temp, int *stack, t_params *params, int length)
 {
-	int	*temp;
-
-	temp = malloc(sizeof(int) * length);
-	if (!temp)
+	while (length > 0)
 	{
-		malloc_fail;
-		return (NULL);
+		stack[length] = stack[length - 1];
+		length--;
 	}
-	params->counter = 0;
-	while (params->counter < length)
-	{
-		if (params->counter == 0)
-			temp[params->counter] = stack_2[params->counter];
-		else
-			temp[params->counter] = stack_1[params->counter - 1];
-		params->counter++;
-	}
-	free (stack_1);
-	return (temp);
+	stack[length] = temp;
 }
 
-int	*push_temp_2(int*stack_2, t_params *params, int length)
+void	push_from(int *stack, t_params *params, int length)
 {
-	int	*temp;
-
-	temp = malloc(sizeof(int) * length);
-	if (!temp)
-	{
-		malloc_fail;
-		return (NULL);
-	}
 	params->counter = 0;
 	while (params->counter < length)
 	{
-		temp[params->counter] = stack_2[params->counter + 1];
+		stack[params->counter] = stack[params->counter + 1];
 		params->counter++;
 	}
-	free (stack_2);
-	return (temp);
 }
 
 void	push(int *stack_1, int *stack_2, char c, t_params *params)
@@ -89,17 +66,25 @@ void	push(int *stack_1, int *stack_2, char c, t_params *params)
 	int	length;
 
 	if (c == 'a')
-		length = params->length_a + 1;
-	else
-		length = params->length_b + 1;
-	stack_1 = push_temp_1(stack_1, stack_2, params, length);
-	if (c == 'a')
-		stack_2 = push_temp_2(stack_2, params, params->length_b - 1);
-	else
-		stack_2 = push_temp_2(stack_2, params, params->length_a - 1);
-	if (c == 'a')
+	{
+		if (stack_2 == NULL)
+			return ;
+		params->length_a = params->length_a + 1;
+		params->length_b = params->length_b - 1;
+		push_to(stack_2[0], stack_1, params, params->length_a);
+		push_from(stack_2, params, params->length_b);
 		write(1, "pa", 3);
+		write(1, '\n', 1);
+	}
 	else
+	{
+		if (stack_1 == NULL)
+			return ;
+		params->length_b = params->length_b + 1;
+		params->length_a = params->length_a - 1;
+		push_to(stack_1[0], stack_2, params, params->length_b);
+		push_from(stack_1, params, params->length_a);
 		write(1, "pb", 3);
-	write(1, '\n', 1);
+		write(1, '\n', 1);
+	}
 }
